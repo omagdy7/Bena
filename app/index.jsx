b81d3a0cf332dbc3d4cd2d43dfb12ece1a6c6b4f
omagdy <omar.professional8777@gmail.com>
2024-12-02 22:00:54 +0200

    feat: Implements basic onboarding screen
    Implements #22

---
+++ app/index.jsx
@@ -0,0 +1,41 @@
+import { ScrollView, View, Image, Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Link, router } from 'expo-router';
+import { images } from '../constants'
+import CustomButton from '../components/CustomButton';
+import { StatusBar } from 'expo-status-bar';
+import './global.css'
+import { ImageBackground } from 'react-native';
+
+export default function App() {
+  return (
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView contentContainerStyle={{ height: '100%' }}>
+        <View className="w-full h-[85vh] justify-center items-center px-4">
+          <Image
+            source={images.logo}
+            className="w-[130px] h-[140px]"
+            resizeMode='contain'
+          />
+          <View className="relative mt-5">
+            <Text className="text-3xl text-white font-bold text-center"> Plan Your Perfect Getaway with{' '}
+              <Text className="text-secondary-200">Bena</Text>
+            </Text>
+          </View>
+          <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">Where creativity meets innovation: embark on a journey of limitless exploration with
+          </Text>
+          <CustomButton
+            title={"Continue with Email"}
+            handlePress={() => router.push('/sign-in')}
+            containerStyles={"w-full mt-7"}
+          />
+
+        </View>
+      </ScrollView>
+
+      <StatusBar backgroundColor='#161622' style='light' />
+    </SafeAreaView>
+  );
+}
+
+
