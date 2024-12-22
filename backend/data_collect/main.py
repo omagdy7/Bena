9216d6a55659485019255444e5ee4b98d852e52a
omerashraf22 <aomerashraf22@gmail.com>
2024-12-22 18:12:43 +0200

    Adding Data scraping intial files

---
+++ backend/data_collect/main.py
@@ -0,0 +1,5 @@
+# This project is responsible for getting places data, cleaning them and exporting them to data completion
+
+# we will use scrappy and other different techniques to get popular and trending places
+
+# it is ok to scrap even only place name and then data completion will handel further searches
