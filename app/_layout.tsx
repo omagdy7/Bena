dd854d9ddd1ddd64b0fb203a006293a8f3170506
omagdy <omar.professional8777@gmail.com>
2024-12-23 05:05:56 +0200

    feat: Added a settings screen and made the onboarding screen more appealing

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -116,6 +116,7 @@ export default function RootLayout() {
           <Stack.Screen name="index" options={{ headerShown: false }} />
           <Stack.Screen name="(auth)" options={{ headerShown: false }} />
           <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="settings" options={{ headerShown: false }} />
           <Stack.Screen name="home/[id]" options={{ headerShown: false }} />
         </Stack>
       </AuthProvider>
