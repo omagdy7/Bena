4758d77b64a64fdf8b69b585d5d5ec1aad6ac900
omagdy <omar.professional8777@gmail.com>
2024-12-31 08:31:40 +0200

    clean: Removed uncessary icons and images

--- constants/images.js
+++ constants/images.js
@@ -1,8 +1,3 @@
-const profile = require("../assets/images/profile.png");
-const cards = require("../assets/images/cards.png");
 const logo = require("../assets/images/logo.png");
-const logoSmall = require("../assets/images/logo-small.png");
-const onBoardingBackground = require("../assets/images/on-boarding-background.jpeg");
-const empty = require("../assets/images/empty.png");
 
-export default { profile, cards, logo, logoSmall, onBoardingBackground, empty };
+export default { logo };
